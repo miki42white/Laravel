4125cec5cd8c9a106d46307d87f7e52c06dddd4f
miki42 <junjishiraishi@shiraishiatsushinoMacBook-ea.local>
2021-08-30 09:34:01 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
 Route::post('/add', [TodoController::class, 'create']);
